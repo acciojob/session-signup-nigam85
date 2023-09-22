4e2b2c352dc038664e9d69afb1fd4b88f91da339
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-22 14:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+ document.getElementById('submit').addEventListener('click', function() {
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      const confirmPassword = document.getElementById('confirm-password').value;
+
+      if (password === confirmPassword) {
+        const userData = {
+          name: name,
+          email: email,
+          password: password
+        };
+
+        // Store data in session storage
+        sessionStorage.setItem('user', JSON.stringify(userData));
+
+        // Show success message
+        alert('Sign up successful!');
+      } else {
+        // Show error message
+        alert('Passwords do not match');
+      }
+    });
+  
